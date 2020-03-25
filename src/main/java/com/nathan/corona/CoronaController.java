04af1fd1a1c36edf8a29d583ee68b941ab7d91e1
nathan <trantrungnhat6196@gmail.com>
2020-03-26 01:10:33 +0700

    MultiThread

--- src/main/java/com/nathan/corona/CoronaController.java
+++ src/main/java/com/nathan/corona/CoronaController.java
@@ -1,10 +1,9 @@
 package com.nathan.corona;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +15,7 @@ public class CoronaController {
     private ServiceCorona serviceCorona;
 
 
-    @PostMapping(value = "/api/corona")
+    @GetMapping(value = "/api/corona")
     public ResponseEntity<InsideText> getCoronaDetails(@RequestBody RequestBodyMapping requestBodyMapping) throws JsonProcessingException {
         System.out.println(requestBodyMapping.getResponse_url());
         serviceCorona.executeAsynchronously(requestBodyMapping.getResponse_url());
